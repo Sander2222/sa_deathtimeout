9c3fc93165762a1fd6fb5b1302663a6cc730bad7
Sander <heinzwalter0987@gmail.com>
2023-07-20 20:42:04 +0200

    hg

--- server/main.lua
+++ server/main.lua
@@ -1,91 +1,25 @@
---made by Sander#2211
+PlayerTimeOuts = {}
 
-ESX = nil
+RegisterServerEvent('esx:onPlayerDeath')
+AddEventHandler('esx:onPlayerDeath', function(data)
+    local DataTable = {}
+    local xPlayer =  ESX.GetPlayerFromId(data.victim)
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    DataTable.Identifier = xPlayer.getIdentifier()
+    DataTable.Time = Config.Timeout
 
---Sendet alle Daten zu Client
-RegisterServerEvent('sa_banking:GetBalance')
-AddEventHandler('sa_banking:GetBalance', function()
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    balance = xPlayer.getAccount('bank').money
-    name = xPlayer.getName()
-    TriggerClientEvent('sa_banking:SendNUI', _source, balance, name, true)
-
+    table.insert(PlayerTimeOuts, DataTable)
 end)
 
---Auszahlen
-RegisterServerEvent('sa_banking:Withdraw')
-AddEventHandler('sa_banking:Withdraw', function(amount)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    local balance = xPlayer.getAccount('bank').money 
-    local amount = tonumber(amount)
-
-    if amount == nil or amount <= 0 then
-      TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Withdraw'], Language['NotValid'], Language['NotifyIcon'], 3)
-    else 
-        if amount <= balance then
-            xPlayer.removeAccountMoney('bank', amount)
-            xPlayer.addMoney(amount)
-            TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Withdraw'], Language['WithdrawMoney'] ..amount.. Language['Money'], Language['NotifyIcon'], 3)
-        else
-          TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Withdraw'], Language['NotEnoughMoney'], Language['NotifyIcon'], 3)
-        end
-    end
-end)
-
-
---Einzahlen
-RegisterServerEvent('sa_banking:Deposit')
-AddEventHandler('sa_banking:Deposit', function(amount)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    local amount = tonumber(amount)
-    local HandMoney = xPlayer.getMoney()
-
-    if amount == nil or amount <= 0 then
-      TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Deposit'], Language['NotValid'], Language['NotifyIcon'], 3)
-    else
-        if amount <= HandMoney then
-            xPlayer.removeMoney(amount)
-            xPlayer.addAccountMoney('bank', tonumber(amount))
-            TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Deposit'], Language['DepositMoney'] ..amount.. Language['Money'], Language['NotifyIcon'], 3)
-        else
-          TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Deposit'], Language['NotEnoughMoney'], Language['NotifyIcon'], 3)
-        end
-    end
-end)
-
-
---Übwerweisen
-RegisterServerEvent('sa_banking:Transfer')
-AddEventHandler('sa_banking:Transfer', function(amount, Target)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    local xTarget = ESX.GetPlayerFromId(Target)
-    local xPlayerBank = xPlayer.getAccount('bank').money
-    amount = tonumber(amount)
-
-     if xTarget ~= nil and GetPlayerEndpoint(Target) ~= nil then
-        --idk why this is working, but it works lol
-        if tonumber(_source) ~= tonumber(Target) then
-            if balance <= 0 or balance < amount or amount <= 0 then
-              TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Transfer'], Language['NotEnoughMoney'], Language['NotifyIcon'], 3)
+CreateThread(function()
+    while true do
+        for k, v in ipairs(PlayerTimeOuts) do
+            if v.Time <= 1 then
+                TriggerClientEvent('sa_timeout:client:RemoveTimeout', source, v.Time)
             else 
-                xPlayer.removeAccountMoney('bank', amount)
-                xTarget.addAccountMoney('bank', amount)
-                TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Transfer'], Language['TransferSource'] ..amount.. Language['Money'], Language['NotifyIcon'], 3)
-                TriggerClientEvent('esx:showAdvancedNotification', Target, Language['Bank'], Language['Transfer'], Language['TransferTarget'] ..amount.. Language['Money'], Language['NotifyIcon'], 3)
+                TriggerClientEvent('sa_timeout:client:UpdateTimeOut', source, v.Time)
             end
-        else 
-          TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Transfer'], Language['TransferToSelf'], Language['NotifyIcon'], 3)
         end
-     else
-      TriggerClientEvent('esx:showAdvancedNotification', _source, Language['Bank'], Language['Transfer'], Language['PlayerNotExists'], Language['NotifyIcon'], 3)
-     end
+        Wait(1000)
+    end
 end)
-
-
-
