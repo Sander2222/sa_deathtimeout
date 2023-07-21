6fdbde0f254f3dedc5fcfe5b27fbc7d398fb5b42
Sander <86840128+Sander2222@users.noreply.github.com>
2023-07-22 01:07:01 +0200

    add locals

--- config.lua
+++ config.lua
@@ -1,3 +1,13 @@
 Config = {}
 
-Config.Timeout = 600 -- In Sekunden angeben 
+Config.Timeout = 30 -- In Sekunden angeben 
+
+Config.RemoveTimeout = 'removetimeout'
+
+Config.Locals = {
+    ['TargetRemoveTimeout'] = 'Dir wurde dein Timeout removed',
+    ['PlayerRemovedTimeout'] = 'Du hast der Spieler ID %s den Timeout entfernt',
+    ['RemovedSelfTimeout'] = 'Du hast dir selber den Timeout entfernt',
+    ['NoPlayerWithTimeOut'] = 'Es gibt keinen Spieler der einen Timeout hat mit dieser ID',
+    ['NoRights'] = 'Du hast dazu keine Rechte'
+}
